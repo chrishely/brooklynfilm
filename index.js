8de8aa72e32d592e1f28bb1558ed4c99adc7339c
Christopher Hely <christopherhely@Christophers-iMac.local>
2019-03-11 22:01:02 +0100

    add commit

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ $(document).ready(function () {
       $bgobj.css({ backgroundPosition: coords });
     });
   });
+// carousel
+  $('.carousel').carousel()
 
+// lightbox JQuery Goonies
   $('.lightbox').click(function () {
 
     $('.backdrop, .box').animate({ 'opacity': '.50' }, 300, 'linear');
@@ -28,10 +31,29 @@ $(document).ready(function () {
   });
 });
 
-// lightbox JQuery
-
 function close_box() {
   $('.backdrop,.box').animate({ 'opacity': '0' }, 300, 'linear', function () {
     $('.backdrop, .box').css('display', 'none');
   });
 }
+// lightbox JQuery Jungle Book
+$('.lightbox1').click(function () {
+
+  $('.backdrop1, .box1').animate({ 'opacity': '.50' }, 300, 'linear');
+  $('.box1').animate({ 'opacity': '1.00' }, 300, 'linear');
+  $('.backdrop1, .box1').css('display', 'block');
+});
+$('.close1').click(function () {
+  close_box1()
+});
+$('.backdrop1').click(function () {
+  close_box1()
+});
+
+
+function close_box1() {
+$('.backdrop1,.box1').animate({ 'opacity': '0' }, 300, 'linear', function () {
+  $('.backdrop1, .box1').css('display', 'none');
+});
+}
+
